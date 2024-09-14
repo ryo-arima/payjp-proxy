ca4e9ef7dba59b3cc82aca6cc0b79605273704f6
Ryo Arima <ryo.arima@outlook.com>
2024-09-15 06:38:55 +0900

    🐿

--- cmd/client/app/main.go
+++ cmd/client/app/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"os"
+
 	"github.com/ryo-arima/payjp-proxy/pkg/client"
 	"github.com/ryo-arima/payjp-proxy/pkg/config"
 )
 
 func main() {
-	conf := config.NewBaseConfig()
-	client.ClientForAppUser(conf)
+	yamlConfig, err := config.LoadYaml(os.Getenv("PAYJP_PROXY_CONFIG_PATH"))
+	if err != nil {
+		panic(err)
+	}
+	conf, err := config.NewBaseConfig(yamlConfig)
+	if err != nil {
+		panic(err)
+	}
+	client.ClientForAppUser(*conf)
 }
